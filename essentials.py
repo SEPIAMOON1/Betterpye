b99bb7b34b3e3f1a692dbbed7b72ebf13d2261c7
SEPIAMOON1 <sepiamoon@proton.me>
2026-08-24 16:03:27 -0300

    X/Twitter support

--- essentials.py
+++ essentials.py
@@ -2,9 +2,17 @@ import tkinter as tk
 
 def fixupurl(text):
     url = text.get()
-    #This will replace the i on "tiktok" turning it into "tnktok". 
-    url = url.replace("i", "n", 1)
-    #I will make one for X/Twitter soon.
+    url = url.strip()
+
+    # This will replace the i on "tiktok" turning it into "tnktok". 
+    if "x.com" in url or "twitter.com" in url:
+        plataform = "X/Twitter"
+        xstart = url.find("x.com" or "twitter.com")
+        url = url[:8] + "fixup" + url[xstart:]
+        
+    elif "tiktok.com" in url:
+        plataform = "Tiktok"
+        url = url.replace("i", "n", 1)
 
     text.delete(0, tk.END)
     text.insert(0, url)
